f3b9bc5f2afea89891af0eee5075018a65cd34df
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-01 13:23:44 +0300

    Adding todo.js with two functions that hide form tasks

---
+++ javascript/todo.js
@@ -0,0 +1,8 @@
+// variable
+var editTaskId = 0;
+
+
+// hide the form 
+$('.form-appear').toggle();
+// when click on button Add a task form appear
+$('#addtask-item').click(() => $('.form-appear').toggle());
